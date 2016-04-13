15587fecfb0de806de0751ddadb9ba7cfd8b473e
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-04-13 15:04:52 +0200

    frontend code from `yo redux`

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# my-new-project (my-new-project)
+
+> ...
+
+## Running your project
+
+The generated project includes a development server on port `3000`, which will rebuild the app whenever you change application code. To start the server (with the dev-tools enabled), run:
+
+```bash
+$ npm start
+```
+
+To run the server with the dev-tools disabled, run:
+
+```bash
+$ DEBUG=false npm start
+```
+
+To build for production, this command will output optimized production code:
+
+```bash
+$ npm run build
+```
---
+++ src/app/webpack.production.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var devFlagPlugin = new webpack.DefinePlugin({
+  __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
+});
+
+module.exports = {
+  entry: [
+    './js/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/static/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    devFlagPlugin,
+    new ExtractTextPlugin('app.css')
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('css-loader?module!cssnext-loader') }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json']
+  }
+};
