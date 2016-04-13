71ff508632fa46936b62c539db37c79270f6b8b3
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-04-13 20:41:27 +0200

    serving static compiled by webpack

--- src/api/views/index.py
+++ src/api/views/index.py
@@ -1,10 +1,29 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint
+import os
+
+from flask import Blueprint, send_from_directory
+from config import SRC_ROOT
+
+
+STATIC_DIR = os.path.join(SRC_ROOT, 'app', 'dist')
+
 
 index_page = Blueprint('index', __name__)
+_index_html = None
+
+
+@index_page.route('/static/<path:filename>')
+def static(filename):
+    return send_from_directory(
+        STATIC_DIR,
+        filename, as_attachment=True)
 
 
 @index_page.route('/')
 @index_page.route('/<path:ignored>')
-def index():
-    return index_page.send_static_file('index.html')
+def index(ignored=None):
+    global _index_html
+    if _index_html is None:
+        with open(os.path.join(SRC_ROOT, 'app', 'index.html')) as index_html:
+            _index_html = index_html.read()
+    return _index_html
